b47c177db36d4e375af25e5474b85d4858992826
kdex <kdex@kdex.de>
2016-05-18 10:55:55 +0200

    Added random server selection

--- src/server/js/Query.js
+++ src/server/js/Query.js
@@ -1,8 +1,13 @@
 import "babel-polyfill";
 import fetch from "node-fetch";
+const servers = [
+	"http://overpass-api.de/api",
+// 	"http://overpass.osm.rambler.ru/cgi"
+];
 export async function execute(query) {
 	const source = query.trim().replace(/\s+/g, "");
-	const url = `http://overpass.osm.rambler.ru/cgi/interpreter?data=[out:json];${source}out body;`;
+	const server = servers[Math.floor(servers.length * Math.random())];
+	const url = `${server}/interpreter?data=[out:json];${source}out body;`;
 	const response = await fetch(url);
 	return await response.json();
 };
